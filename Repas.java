250dd94d318a14e3106ac2e02122c8388e546175
gautierlef <gautier.leffebvre.sio@gmail.com>
2019-10-08 14:58:55 +0200

    Ajout constructeur de la classe Repas

--- Repas.java
+++ Repas.java
@@ -13,4 +13,9 @@ import java.time.LocalDate;
 public class Repas {
     private int numero;
     private LocalDate date;
+
+    public Repas(int numero, LocalDate date) {
+        this.numero = numero;
+        this.date = date;
+    }
 }
